04305446dd94606771b431a9dcbf175963249e59
fuckinrobotics <60086412+fuckinrobotics@users.noreply.github.com>
2023-03-09 23:40:24 +0300

    Update task2.m

--- task2.m
+++ task2.m
@@ -1,13 +1,24 @@
-% Параметры
-load DSPhsound.mat;
-fc = 100000;
+% загрузка файла
+load('DSPhsound.mat'); % загрузка файла
+
+% входные параметры
+Fc = 1150;
 ripple = 3;
 n = 5;
-Wn = fc/(Fs/2);
-wp = 2*Fs*tan(pi*Wn/Fs);
-[b, a] = butter(n, wp, 's');
-[bl, al] = lp2lp(b, a, Wn);
+
+% расчет коэффициентов фильтра Баттерворта
+[b, a] = butter(5, 2*Fc/Fs, 'low', 's');
+
+% предварительное деформирование частоты
+Wc = 2*Fs*tan(pi*Fc/Fs);
+
+% преобразование ФНЧ-фильтра Баттерворта в ФНЧ-фильтр заданного типа
+[bl, al] = lp2lp(b, a, Wc);
+
+% билинейное преобразование для получения дискретного аналога
 [bz, az] = bilinear(bl, al, Fs);
+
+% округление
 bz = round(bz*1e4)/1e4;
 az = round(az*1e4)/1e4;
 
@@ -15,19 +26,19 @@ az = round(az*1e4)/1e4;
 [z, p, k] = tf2zpk(bz, az);
 zplane(z, p);
 if any(abs(p) >= 1)
-    disp('Фильтр неустойчив');
+   disp('Фильтр неустойчив изначально');
 else
-    disp('Фильтр устойчив');
+   disp('Фильтр устойчив изначально');
 end
 
 % Продолжаем округлять и следим за устойчивостью фильтра
 for i = 3:-1:0
-    bz = round(bz*10^i)/10^i;
-    az = round(az*10^i)/10^i;
-    [z, p, k] = tf2zpk(bz, az);
-    zplane(z, p);
-    if any(abs(p) >= 1)
-        disp(['Фильтр неустойчив при округлении до ', num2str(i), ' знаков']);
-        break;
-    end
+   bz = round(bz*10^i)/10^i;
+   az = round(az*10^i)/10^i;
+   [z, p, k] = tf2zpk(bz, az);
+   zplane(z, p);
+   if any(abs(p) >= 1)
+       disp(['Фильтр неустойчив при округлении до ', num2str(i), ' знаков']);
+       break;
+   end
 end
